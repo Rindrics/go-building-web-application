22284d1f964cba7cb5b4c8f8e2263995bb6ad5fd
Akira Hayashi <smxshxishxad@me.com>
2022-06-14 08:45:58 +0900

    :sparkles: Serve files according to request path

---
+++ ch2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	fmt.Println(pageID)
+	fileName := "files/" + pageID + ".html"
+	http.ServeFile(w, r, fileName)
+}
+
+func main() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	http.Handle("/", rtr)
+	http.ListenAndServe(PORT, nil)
+}
