3316663127fc7d733a04a54b32b3733064d860bc
Akira Hayashi <smxshxishxad@me.com>
2022-06-15 06:46:05 +0900

    :wrench: Generate page content from db entry

--- ch3/main.go
+++ ch3/main.go
@@ -4,6 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -12,6 +16,7 @@ const (
 	DBUser  = "root"
 	DBPass  = "password!"
 	DBDbase = "cms"
+	PORT    = ":8080"
 )
 
 var database *sql.DB
@@ -22,6 +27,22 @@ type Page struct {
 	Date    string
 }
 
+func ServePage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	thisPage := Page{}
+	fmt.Println(pageID)
+
+	err := database.QueryRow("SELECT page_title,page_content,page_date FROM pages WHERE id=?", pageID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
+	if err != nil {
+		log.Println("Couldn't get page: +pageID")
+		log.Println(err.Error)
+	}
+
+	html := `<html><head><title>` + thisPage.Title + `</title></head><body><h1>` + thisPage.Title + `</h1><div>` + thisPage.Content + `</div></body></html>`
+	fmt.Fprintln(w, html)
+}
+
 func main() {
 	dbConn := fmt.Sprintf("%s:%s@tcp(%s)/%s", DBUser, DBPass, DBHost, DBDbase)
 	db, err := sql.Open("mysql", dbConn)
@@ -30,4 +51,9 @@ func main() {
 		log.Println(err.Error)
 	}
 	database = db
+
+	routes := mux.NewRouter()
+	routes.HandleFunc("/page/{id:[0-9]+}", ServePage)
+	http.Handle("/", routes)
+	http.ListenAndServe(PORT, nil)
 }
