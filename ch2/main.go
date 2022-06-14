5e585dbe039a53d05852a1b7adecb7d0aa2cca4b
Akira Hayashi <smxshxishxad@me.com>
2022-06-14 09:15:42 +0900

    :sparkles: Serve 404.html for invalid paths

--- ch2/main.go
+++ ch2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -16,11 +17,16 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	pageID := vars["id"]
 	fmt.Println(pageID)
 	fileName := "files/" + pageID + ".html"
+	_, err := os.Stat(fileName)
+	if err != nil {
+		fileName = "files/404.html"
+	}
 	http.ServeFile(w, r, fileName)
 }
 
 func main() {
 	rtr := mux.NewRouter()
+	rtr.HandleFunc("/{id}", pageHandler)
 	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
 	rtr.HandleFunc("/{id:homepage}", pageHandler)
 	rtr.HandleFunc("/{id:contact}", pageHandler)
