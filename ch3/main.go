973d1ac6d5afa1c071e4ade10eadaf712b6ac8a6
Akira Hayashi <smxshxishxad@me.com>
2022-06-18 02:16:52 +0900

    :sparkles: Handle request using GUID

--- ch3/main.go
+++ ch3/main.go
@@ -29,13 +29,14 @@ type Page struct {
 
 func ServePage(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	pageID := vars["id"]
+	pageGUID := vars["guid"]
 	thisPage := Page{}
-	fmt.Println(pageID)
+	fmt.Println(pageGUID)
 
-	err := database.QueryRow("SELECT page_title,page_content,page_date FROM pages WHERE id=?", pageID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
+	err := database.QueryRow("SELECT page_title,page_content,page_date FROM pages WHERE page_guid=?", pageGUID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
 	if err != nil {
-		log.Println("Couldn't get page: +pageID")
+		log.Println("Couldn't get page: " + pageGUID)
+		log.Println(pageGUID)
 		log.Println(err.Error)
 	}
 
@@ -53,7 +54,7 @@ func main() {
 	database = db
 
 	routes := mux.NewRouter()
-	routes.HandleFunc("/page/{id:[0-9]+}", ServePage)
+	routes.HandleFunc("/page/{guid:[0-9a-zA\\-]+}", ServePage)
 	http.Handle("/", routes)
 	http.ListenAndServe(PORT, nil)
 }
